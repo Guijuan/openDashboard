62c5865ace529de0165c315766d3b619d3c80cfe
zhaoweixin <627035095@qq.com>
2022-06-07 14:40:22 +0800

    2rd

---
+++ client/src/common/NavBar/NavBar.vue
@@ -0,0 +1,34 @@
+
+<template>
+<vs-navbar class="nabarx" color='rgb(50, 60, 90)'>
+      <div slot="title" style="">
+        <vs-navbar-title style="color:white;">
+        OpenVIS 
+        </vs-navbar-title>
+      </div>
+      
+      <vs-spacer></vs-spacer>
+
+      <vs-navbar-item index="0" style="color:white; padding-right:5px">
+        <a href="http://localhost:8080/home">Home</a>
+      </vs-navbar-item> 
+      <vs-navbar-item index="1" style="color:white; padding-right:5px">
+        <a href="http://localhost:8080/blue">Editor</a>
+      </vs-navbar-item>
+</vs-navbar>
+</template>
+<script>
+    export default{
+
+        name:"NavBar",
+        data: function(){
+          return{
+          }
+        }
+    }
+</script>
+<style>
+li{
+  margin: 0.1% !important
+}
+</style>
---
+++ client/src/pages/ViewLayouts/TemplateA.vue
@@ -0,0 +1,195 @@
+<template>
+    <div class="layout">
+        <div class="header">
+            System Preview
+        </div>
+        <div class="mutiItem">
+            <div class="left">
+                <div id="t1">
+                    <div id="chartA"></div>
+                </div>
+                <div id="t2">
+                    <div id="chartC"></div>
+                </div>
+            </div>
+            <div class="right">
+                <div id="t3">
+                    <div id="chartB"></div>
+                </div>
+                <div id='t4'>
+                    <div id="chartD"></div>
+                </div>
+            </div>
+            <!--图表 4-->
+        </div>
+    </div>
+</template>
+
+<script>
+
+import vegaEmbed from "vega-embed";
+import * as d3 from "d3";
+
+export default{
+    data() {
+        return {
+            ModularInfo:{},
+            chartStyle:{"chartA":{"width": 828,"height": 390},
+                    "chartB":{"width": 828,"height": 390},
+                    "chartC":{"width": 828,"height": 390},
+                    "chartD":{"width": 828,"height": 390}
+                    },
+            layoutObj:{}
+        }
+    },
+    props:["layout"],
+    components: {
+        //图表组件
+        //chartA,
+        //chartB,
+        //chartC,
+        //chartD
+    },
+    watch:{
+        layout:{
+            handler(curval){
+                console.log(curval)
+            },
+            deep: true            
+        }
+    },
+    mounted(){
+    },
+    methods:{
+        getModularInfo(m){
+            let that = this
+            this.layoutObj = JSON.parse(JSON.stringify(m))
+            console.log(this.layoutObj)
+            //this.calculateChartWH()
+            //this.setBackgroundColor()
+            this.adaptWidthHeight()
+            this.generateGraph()
+            
+        },
+        adaptWidthHeight(){
+            //this.layoutObj.[chartA].data.height/width
+            let that = this
+            let chartList = Object.keys(that.chartStyle)
+            chartList.forEach(function(d){
+                if(that.layoutObj["config"][d] != undefined){
+                    //用vega model 自带的set方法
+                    that.layoutObj["config"][d]["data"]["height"] = that.chartStyle[d]["height"]
+                    that.layoutObj["config"][d]["data"]["width"] = that.chartStyle[d]["width"]
+                    let _layer = that.layoutObj["config"][d]["data"]["layer"]
+                    //console.log("_layer", _layer)
+                    _layer.forEach(function(v){
+                        v.height = that.chartStyle[d]["height"]
+                        v.width = that.chartStyle[d]["width"]
+                    })
+                }
+            })
+            
+        },
+        setBackgroundColor(){
+            let that = this
+            for(let key in that.layoutObj["config"]){
+                //key chartA chartB
+                that.layoutObj["config"][key]["data"]["background"] = "#242E47"
+            }
+        },
+        setAxisColor(){
+            let that = this
+            for(let key in that.layoutObj["layers"]){
+                //key chartA chartB
+                if(that.layoutObj["layers"][key]["encoding"]["x"]){
+                    if(!that.layoutObj["layers"][key]["encoding"]["x"]["axis"]){
+                        that.layoutObj["layers"][key]["encoding"]["x"]["axis"] = {
+                            "domainColor" : "white",
+                            "labelColor": "white",
+                            "tickColor": "white",
+                            "titleColor": "white",
+                            "gridColor": "white"
+                        }
+                    }
+                }
+                if(that.layoutObj["layers"][key]["encoding"]["y"]){
+                    if(!that.layoutObj["layers"][key]["encoding"]["y"]["axis"]){
+                        that.layoutObj["layers"][key]["encoding"]["y"]["axis"] = {
+                            "domainColor" : "white",
+                            "labelColor": "white",
+                            "tickColor": "white",
+                            "titleColor": "white",
+                            "gridColor": "white"
+                        }
+                    }
+                }
+            }
+        },
+        setHeaderColor(){
+
+        },
+        generateGraph(){
+            let that = this
+            let charts = Object.keys(that.layoutObj["config"])
+            charts.forEach(function(d){
+                vegaEmbed("#" + d, that.layoutObj["config"][d]["data"])
+            })
+        }
+    }
+    
+}
+
+</script>
+<style type="text/css">
+
+.layout{
+    width: 1800px;
+    margin: 0 auto;
+    height: 1000px;
+    background-color: #171C30;
+    border-radius:5px
+}
+.header{
+	padding: 0.5% 0 0.5% 2%;
+    font-size: 25px;
+    color:white;
+    border-radius:5px;
+}
+.mutiItem{
+    height: 95%;
+    background-color: white;
+}
+.left{
+    width: 50%;
+    height: 100%;
+    float: left;
+}
+.right{
+    width: 50%;
+    height: 100%;
+    float: left;
+}
+#chartA,#chartB,#chartC,#chartD{
+    height: 100% ;
+    border-radius: 3px;
+    box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
+}
+
+#t1,#t2,#t3,#t4{
+	height:46%;
+	width:96%;
+	padding:2%
+}
+
+</style>
+<style media="screen">
+    /* Add space between Vega-Embed links  */
+    .vega-actions a {
+        margin-right: 5px;
+    }
+</style>
+
+
+
+
+
