750e992bca8361c402616918dcff8bf1b2011207
guohao225 <guohao225@163.com>
2023-06-21 15:20:24 +0800

    修改推荐

--- client/src/common/NavBar/NavBar.vue
+++ client/src/common/NavBar/NavBar.vue
@@ -31,24 +31,28 @@
           <h4>Background</h4>
           <div style="display: flex;height: 60px;padding: 5px;border: 1px solid;
           background: #f2f2f2;border-radius: 5px;flex-flow: wrap;">
-            <div class="color-item" v-for="(item,index) in preDefineColor" :key="index" :style="{background:item}"></div>
+            <div class="color-item" v-for="(item,index) in preDefineColor" :key="index"
+                 :style="{background:item.color,opacity:item.select?1:0.5}"
+                 @click="visPaletteSetting(item, 1)"></div>
           </div>
           <h4>Other</h4>
           <div style="display: flex;margin-bottom: 0.5rem;height: 60px;padding: 5px;border: 1px solid;
           background: #f2f2f2;border-radius: 5px;flex-flow: wrap;">
-            <div class="color-item" v-for="(item,index) in preDefineColor" :key="index" :style="{background:item}"
-                 @click="visPaletteSetting(1)"></div>
+            <div class="color-item" v-for="(item,index) in preDefineColor" :key="index" :style="{background:item.color,opacity:item.select?1:0.5}"
+                 @click="visPaletteSetting(item, 2)"></div>
           </div>
-          <vs-button style="width: 150px;height: 30px;position: absolute;right: 10px" color="success">Use In My Design</vs-button>
+          <vs-button style="width: 150px;height: 30px;position: absolute;right: 10px" color="success" @click="pushSet(1)">Use In My Design</vs-button>
         </div>
         <vs-divider></vs-divider>
         <h4>Visual Forms</h4>
         <div style="padding-left: 10px;height: 80px">
           <div style="display: flex;margin-bottom: 0.5rem;height: 60px;border: 1px solid;padding: 5px;
           background: #f2f2f2;border-radius: 5px;flex-flow: wrap;">
-            <div class="color-item" v-for="(item,index) in preDefineGrid" style="width: auto" :key="index">{{item}}</div>
+            <div class="color-item" v-for="(item,index) in preDefineGrid"
+                 :style="{width:'auto',opacity: item.select?1:0.5}" :key="index"
+                 @click="visPaletteSetting(item, 3)">{{item.chart}}</div>
           </div>
-          <vs-button style="width: 150px;height: 30px;position: absolute;right: 10px" color="success">Use In My Design</vs-button>
+          <vs-button style="width: 150px;height: 30px;position: absolute;right: 10px" color="success" @click="pushSet(2)">Use In My Design</vs-button>
         </div>
       </div>
       </vs-dropdown-menu>
@@ -63,16 +67,39 @@ export default {
   name: "NavBar",
   data: function () {
     return {
-      colorPanelState:false,
-      gridPanelState:false,
-
-      preDefineColor:['red', 'green', 'blue','write'],
-      preDefineGrid:['templateA','templateB']
+      preDefineColor:[{'color':'red',"select":false},
+        {'color':'green',"select":false},
+        {'color':'blue', "select":false},
+        {'color':'write', "select":false}],
+      preDefineGrid:[{"chart":'templateA', "select":false},{"chart":'templateB', "select":false}],
+      colors:[],
+      others:[],
+      forms:[],
     }
   },
   methods:{
-    visPaletteSetting(type){
-
+    visPaletteSetting(item, type){
+      item.select = !item.select
+      if(item.select == true){
+        switch (type){
+          case 1:this.colors.push(item.color);break
+          case 2:this.others.push(item.color);break
+          case 3:this.forms.push(item.chart);break
+        }
+      }else {
+        switch (type){
+          case 1:this.colors = this.colors.filter(e=>{return e.color!=item.color});break
+          case 2:this.others = this.others.filter(e=>{return e.color!=item.color});break
+          case 3:this.forms = this.forms.filter(e=>{return e.chart!=item.chart});break
+        }
+      }
+    },
+    pushSet(type){
+      switch (type){
+        case 1:this.$store.state.preChartStyle.visPalette.Color.Background = this.colors.map(item=>{return item.color});
+          this.$store.state.preChartStyle.visPalette.Color.Other = this.others.map(item=>{return item.color});break;
+        case 2:this.$store.state.preChartStyle.visualForms = this.forms.map(item=>{return item.chart})
+      }
     }
   }
 }
